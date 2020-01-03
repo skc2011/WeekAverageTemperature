0d3129f10a38a311e23ffc4d16aac534052c15fc
Eugene Kapustynskyi <eugenekapustynskyi@gmail.com>
2020-01-03 22:11:06 +0200

    Initial Commit

---
+++ WeekAverageTemperature/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeekAverageTemperature
+//
+//  Created by SKC on 1/3/20.
+//  Copyright © 2020 SKC-PRO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
